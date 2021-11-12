c8eb63bf47be4a9ed7ff51b70b5c9f754197a8af
Hugh Hudson <hch28@cam.ac.uk>
2021-11-12 20:39:56 +0000

    added other operators

--- index.js
+++ index.js
@@ -4,10 +4,26 @@ var ans;
 
 console.log('Welcome to the calculator');
 
+console.log('Enter operator:');
+const operator = readline.prompt();
+
 console.log('Enter first number:');
 const first_num = readline.prompt();
 
 console.log('Enter second number:');
 const second_num = readline.prompt();
 
-console.log('%s * %s = %s',first_num,second_num,first_num*second_num)
+if (operator == '*'){
+    ans = first_num*second_num;
+}   else if (operator == '/'){
+    ans = first_num/second_num;
+}   else if (operator == '+'){
+    ans = parseFloat(first_num)+parseFloat(second_num);
+}   else if (operator == '-'){
+    ans = first_num-second_num;
+}   else{
+    console.error('Incompatible operator')
+}
+
+console.log('%s %s %s = %s',first_num,operator,second_num,ans)
+
