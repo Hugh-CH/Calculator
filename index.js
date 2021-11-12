5135696fd396d5ef37f58e7718134c9b973a371d
Hugh Hudson <hch28@cam.ac.uk>
2021-11-12 20:10:39 +0000

    added multiplier

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Welcome to the calculator')
+const readline = require('readline-sync');
+
+var ans;
+
+console.log('Welcome to the calculator');
+
+console.log('Enter first number:');
+const first_num = readline.prompt();
+
+console.log('Enter second number:');
+const second_num = readline.prompt();
+
+console.log('%s * %s = %s',first_num,second_num,first_num*second_num)
